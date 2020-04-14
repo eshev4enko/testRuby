7d6f073d101bcbc3bf8fdb35a3dabf597d41da3d
Eugen <shevchenko2786@gmail.com>
2020-04-14 22:45:11 +0300

    db

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,15 @@
 <?php
 //database_connection.php
 
-$connect = new PDO("mysql:host=ec2-46-137-177-160.eu-west-1.compute.amazonaws.com;dbname=d5rtbedq9bdrfc", "fqejpaaxlapkgo", "2425cd44adfb03a257d8106c2a20d29342f76571433e09f18cff277b8eeb2e86
-");
+//$connect = new PDO("mysql:host=ec2-46-137-177-160.eu-west-1.compute.amazonaws.com;dbname=d5rtbedq9bdrfc", "fqejpaaxlapkgo", "2425cd44adfb03a257d8106c2a20d29342f76571433e09f18cff277b8eeb2e86
+////");
+
+$host='ec2-46-137-177-160.eu-west-1.compute.amazonaws.com';
+$db = 'd5rtbedq9bdrfc';
+$username = 'fqejpaaxlapkgo';
+$password = '2425cd44adfb03a257d8106c2a20d29342f76571433e09f18cff277b8eeb2e86';
+
+$connect = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
 
 session_start();
 
