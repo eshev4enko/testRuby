b94a590f88212f2f894ed8171045253e1288e2c2
Eugen <shevchenko2786@gmail.com>
2020-04-14 22:35:35 +0300

    db

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,8 @@
 <?php
 //database_connection.php
 
-$connect = new PDO("mysql:host=localhost;dbname=ruby", "root", "");
+$connect = new PDO("mysql:host=ec2-46-137-177-160.eu-west-1.compute.amazonaws.com;dbname=d5rtbedq9bdrfc", "fqejpaaxlapkgo", "2425cd44adfb03a257d8106c2a20d29342f76571433e09f18cff277b8eeb2e86
+");
 
 session_start();
 
