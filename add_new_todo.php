bd217e437c1c10928782680f633863a63d49b737
Eugen <shevchenko2786@gmail.com>
2020-04-27 19:55:33 +0300

    changes

--- add_new_todo.php
+++ add_new_todo.php
@@ -8,7 +8,7 @@ $data = array(
     ':title_text' => $_POST["title_text"]
 );
 
-//if ($_POST["title_text"] == '') {
+if ($_POST["title_text"] == '') {
     $query = "INSERT INTO task_title (title_text) VALUES (:title_text) ";
 
     $statement = $connect->prepare($query);
@@ -49,6 +49,6 @@ $data = array(
                 </div>
             </div>
         ';
-//    }
+    }
 }
 
